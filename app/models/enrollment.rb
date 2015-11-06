684dd1f1322348a6f2f26aefc03af2f95e17ccd1
Mikhail Dronov <mike@dronov.net>
2015-11-06 15:25:21 +0300

    new portions of code changings

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,9 +1,9 @@
 class Enrollment < ActiveRecord::Base
   belongs_to :course
   belongs_to :user
-  
+
   def user_has_enrollments
-    enrollments = Enrollment.where(course_id: self.course.id, user_id: self.user.id) 
-    enrollments.size > 0
+    enrollments = Enrollment.where(course_id: self.course.id, user_id: self.user.id)
+    enrollments.exists?
   end
 end
