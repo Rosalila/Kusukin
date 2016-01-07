102766001f116bb352ed7e5c23527a780223fcb0
Mikhail Dronov <mike@dronov.net>
2016-01-08 00:21:44 +0300

    fix error in tests with index on email field at user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609153332) do
+ActiveRecord::Schema.define(version: 20160107211656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,7 +129,6 @@ ActiveRecord::Schema.define(version: 20150609153332) do
     t.string   "name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "achievements", "courses"
