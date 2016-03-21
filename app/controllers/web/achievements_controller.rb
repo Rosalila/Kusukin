c41de4ca4b32dc8adc8b8ee4eae7292d3321e38e
Turupawn <ahmed.hn.43@gmail.com>
2016-03-21 15:16:35 -0600

    creating and editiing achievements from the course views

--- app/controllers/web/achievements_controller.rb
+++ app/controllers/web/achievements_controller.rb
@@ -69,6 +69,6 @@ class Web::AchievementsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def achievement_params
-      params.require(:achievement).permit(:title, :user_id, :course_id)
+      params.require(:achievement).permit(:name, :description, :icon, :course_id)
     end
 end
