5ac0ec0690f20649f2765679a1753b56029153da
Mikhail Dronov <mike@dronov.net>
2016-03-23 10:38:49 +0300

    little refactoring

--- app/controllers/web/backend_controller.rb
+++ app/controllers/web/backend_controller.rb
@@ -3,6 +3,8 @@ class Web::BackendController < ApplicationController
   layout 'backend/dashboard'
 
   def courses
+    @courses = Course.where(user_id: current_user.id).order('created_at ASC')
+    @course = Course.find_by_id(params[:course_id])
   end
 
   def balance
