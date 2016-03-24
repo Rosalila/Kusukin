15214ef795d39f4034c47d56d5e168033e8d64c1
Turupawn <ahmed.hn.43@gmail.com>
2016-03-23 18:36:51 -0600

    more fixes

--- app/controllers/web/backend_controller.rb
+++ app/controllers/web/backend_controller.rb
@@ -3,8 +3,12 @@ class Web::BackendController < ApplicationController
   layout 'backend/dashboard'
 
   def courses
-    @courses = Course.where(user_id: current_user.id).order('created_at ASC')
-    @course = Course.find_by_id(params[:course_id])
+    if params[:course_id]
+      @courses = Course.where(user_id: current_user.id).order('created_at ASC')
+      @course = Course.find_by_id(params[:course_id])
+    else
+      @course = Course.new
+    end
   end
 
   def balance
