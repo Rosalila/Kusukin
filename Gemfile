45ad16defd93e2dc40dda71671199a6a5a049043
Turupawn <ahmed.hn.43@gmail.com>
2016-03-24 02:58:43 -0600

    Cors are now enabled

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'rack-cors', :require => 'rack/cors'
+
 gem "refile", require: "refile/rails"
 gem "refile-mini_magick"
 
--- app/controllers/web/javascript_api_controller.rb
+++ app/controllers/web/javascript_api_controller.rb
@@ -2,10 +2,7 @@ class Web::JavascriptApiController < ApplicationController
   protect_from_forgery with: :null_session
 
   def test
-    #n = Nya.new
-    #n.nyu=params[:a]
-    #n.save
-    render json: {"f":"fdsa","player1_wins":10,"player2_wins":20}
+    render json: {"result":"You are connected to Kusukin"}
   end
 
   def save_progress
@@ -21,14 +18,7 @@ class Web::JavascriptApiController < ApplicationController
   end
 
   def get_progress
-    player1_wins=0
-    player2_wins=0
     ucd=UserCourseStorage.where(:user_id=>params[:user_id],:course_id=>params[:course_id])[0]
-    if ucd
-      player1_wins=JSON.parse(ucd.json_data)["player1_wins"].to_i
-      player2_wins=JSON.parse(ucd.json_data)["player2_wins"].to_i
-    end
-
-    render json: {"player1_wins":player1_wins,"player2_wins":player2_wins}
+    render json: ucd.json_data
   end
 end
