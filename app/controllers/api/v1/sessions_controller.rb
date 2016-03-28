3ea68c8112a6117bd8f9bfec0efba2f2740f22d6
Mikhail Dronov <mike@dronov.net>
2016-03-29 00:24:38 +0400

    fix sign out

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,15 +1,16 @@
 class Api::V1::SessionsController < Devise::SessionsController
-  skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_signed_out_user, :only => [:destroy]
 
   def create
     user = warden.authenticate!(params[:session])
     sign_in(resource_name, user)
 
-    current_user.update authentication_token: nil
+    current_user.authentication_token = nil
+    current_user.save
 
     respond_to do |format|
       format.json {
-        render :json => {
+        render json: {
           user: current_user,
           status: :ok,
           authentication_token: current_user.authentication_token
@@ -20,10 +21,12 @@ class Api::V1::SessionsController < Devise::SessionsController
 
   def destroy
     respond_to do |format|
+      user = User.find_by authentication_token: params[:authentication_token]
       format.json {
-        if current_user
-          current_user.update authentication_token: nil
-          signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+        if user
+          user.authentication_token = nil
+          user.save
+          sign_out(user)
           render nothing: true, status: :ok
         else
           render json: nil, status: :unprocessable_entity
