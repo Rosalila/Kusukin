04892da3c65263967e5bd337b6999385f8ee4d0a
Mikhail Dronov <mike@dronov.net>
2016-03-28 23:09:22 +0400

    remove debugs

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,13 +2,7 @@ class Api::V1::SessionsController < Devise::SessionsController
   skip_before_filter :verify_authenticity_token
 
   def create
-    Rails.logger.debug { "======" }
-    Rails.logger.debug { auth_options }
-    Rails.logger.debug { "======" }
     user = warden.authenticate!(params[:session])
-    Rails.logger.debug { "======" }
-    Rails.logger.debug { user }
-    Rails.logger.debug { "======" }
     sign_in(resource_name, user)
 
     current_user.update authentication_token: nil
@@ -30,9 +24,9 @@ class Api::V1::SessionsController < Devise::SessionsController
         if current_user
           current_user.update authentication_token: nil
           signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-          render :json => {}.to_json, status: :ok
+          render nothing: true, status: :ok
         else
-          render :json => {}.to_json, status: :unprocessable_entity
+          render json: nil, status: :unprocessable_entity
         end
       }
     end
