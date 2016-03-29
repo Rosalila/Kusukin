42736ee810881d1c3a0a866584cbbcbd936d225e
Mikhail Dronov <mike@dronov.net>
2016-03-29 13:54:23 +0400

    little refactoring

--- app/controllers/api/v1/achievements_controller.rb
+++ app/controllers/api/v1/achievements_controller.rb
@@ -5,17 +5,17 @@ class Api::V1::AchievementsController < Api::V1::ApplicationController
     user_id = current_user.id
     achievement_id = params[:achievement_id]
 
-    if User.exists?(id: user_id)==false
-      render json: {"error": "User does not exists"}
+    unless User.exists?(id: user_id)
+      render json: { error: 'User does not exists' }
       return
     end
-    if Achievement.exists?(id: achievement_id)==false
-      render json: {"error": "Achievement does not exists"}
+    unless Achievement.exists?(id: achievement_id)
+      render json: { error: 'Achievement does not exists' }
       return
     end
 
-    user_achievement = UserAchievement.where(:user_id=>user_id,:achievement_id=>achievement_id)[0]
-    if !user_achievement
+    user_achievement = UserAchievement.find_by(user_id: user_id, achievement_id: achievement_id)
+    unless user_achievement
       user_achievement = UserAchievement.new
       user_achievement.user_id = user_id
       user_achievement.achievement_id = achievement_id
@@ -23,31 +23,32 @@ class Api::V1::AchievementsController < Api::V1::ApplicationController
     user_achievement.is_unlocked = true
     user_achievement.save
 
-    render json: {"status": "success"}
+    render json: { status: 'success' }
   end
 
   def index
     user = User.find_by_id(params[:id])
 
-    if !user
-      render json: {"error": "User does not exists"}
+    unless user
+      render json: { error: 'User does not exists' }
       return
     end
 
-    achievements=[]
+    achievements = []
     user.achievements.each do |achievement|
       achievements.push({id: achievement.id,
-                          name: achievement.name,
-                          description: achievement.description
-                          #icon: Refile.attachment_url(achievement, :icon, format: "png")
+                        name: achievement.name,
+                        description: achievement.description
+                        #icon: Refile.attachment_url(achievement, :icon, format: "png")
                         })
     end
 
-    render json: {"achievements": achievements}
+    render json: { "achievements": achievements }
   end
 
   private
-    def course_params
-      params.require(:course).permit(:name, :description)
-    end
+
+  def course_params
+    params.require(:course).permit(:name, :description)
+  end
 end
--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -4,16 +4,20 @@ class Api::V1::CoursesController < Api::V1::ApplicationController
   def save
     @course = Course.new(course_params)
     @course.save
+
+    render json: nil, status: :ok
   end
 
   def get
     @course = Course.find_by(id: params[:course_id])
+    render json: nil, status: :ok
 
     render json: nil, status: :unprocessable_entity unless @course
   end
 
   private
-    def course_params
-      params.require(:course).permit(:name, :description)
-    end
+
+  def course_params
+    params.require(:course).permit(:name, :description)
+  end
 end
