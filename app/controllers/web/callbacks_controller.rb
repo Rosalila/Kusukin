9ed8b8dbe4f75d8d514f4141787e29f4737eb841
Turupawn <ahmed.hn.43@gmail.com>
2016-04-25 10:37:52 -0600

    external facebook login test

--- app/controllers/web/callbacks_controller.rb
+++ app/controllers/web/callbacks_controller.rb
@@ -4,7 +4,7 @@ class Web::CallbacksController < Devise::OmniauthCallbacksController
     params = request.env["omniauth.params"]
     login_origin = params["login_origin"]
     if login_origin
-      sign_in_and_redirect login_origin
+      redirect_to login_origin
     else
       sign_in_and_redirect @user
     end
