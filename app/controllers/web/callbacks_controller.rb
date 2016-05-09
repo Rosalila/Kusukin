76d7fe952fd2eb12ff54ef84c391c790892fc1b3
Turupawn <ahmed.hn.43@gmail.com>
2016-05-08 22:50:38 -0600

    Returning the course progress in the facebook login

--- app/controllers/web/callbacks_controller.rb
+++ app/controllers/web/callbacks_controller.rb
@@ -1,10 +1,11 @@
 class Web::CallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    @ucd = UserCourseStorage.find_by(course_id: params[:course_id],user_id: @user.id)
     params = request.env["omniauth.params"]
     login_origin = params["login_origin"]
     if login_origin
-      redirect_to login_origin+"?user_email="+@user.email+"&user_token="+@user.authentication_token+"&current_level="+"0"
+      redirect_to login_origin+"?user_email="+@user.email+"&user_token="+@user.authentication_token+"&current_level="+"0"+"&progress="+@ucd.json_data
     else
       sign_in_and_redirect @user
     end
