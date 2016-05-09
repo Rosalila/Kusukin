3902260e25799fff308b37510a974a00315ab365
Turupawn <ahmed.hn.43@gmail.com>
2016-05-08 22:54:34 -0600

    fixing the course id parameter retrival

--- app/controllers/web/callbacks_controller.rb
+++ app/controllers/web/callbacks_controller.rb
@@ -1,9 +1,10 @@
 class Web::CallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    @ucd = UserCourseStorage.find_by(course_id: params[:course_id],user_id: @user.id)
     params = request.env["omniauth.params"]
     login_origin = params["login_origin"]
+    course_id = params["course_id"]
+    @ucd = UserCourseStorage.find_by(course_id: course_id,user_id: @user.id)
     if login_origin
       redirect_to login_origin+"?user_email="+@user.email+"&user_token="+@user.authentication_token+"&current_level="+"0"+"&progress="+@ucd.json_data
     else
