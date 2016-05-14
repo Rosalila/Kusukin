f919e06410795cb835cf0454cb183274947aceb9
Turupawn <ahmed.hn.43@gmail.com>
2016-05-14 13:49:18 -0600

    google bounce with token

--- app/controllers/web/callbacks_controller.rb
+++ app/controllers/web/callbacks_controller.rb
@@ -20,6 +20,20 @@ class Web::CallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    params = request.env["omniauth.params"]
+    login_origin = params["login_origin"]
+    course_id = params["course_id"]
+    @user.authentication_token=nil
+    @user.save
+    if login_origin
+      @ucd = UserCourseStorage.find_by(course_id: course_id,user_id: @user.id)
+      if @ucd 
+        redirect_to login_origin+"?user_email="+@user.email+"&user_token="+@user.authentication_token+"&current_level="+"0"+"&progress="+@ucd.json_data
+      else
+        redirect_to login_origin+"?user_email="+@user.email+"&user_token="+@user.authentication_token+"&current_level="+"0"
+      end
+    else
+      sign_in_and_redirect @user
+    end
   end
 end
