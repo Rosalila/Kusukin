7e78f1922f8dd9539afb32eeb12531ed355428ec
Turupawn <ahmed.hn.43@gmail.com>
2016-05-15 15:10:54 -0600

    trying to fix login with same email

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(email: auth.info.email).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
